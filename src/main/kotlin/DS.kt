f2148ffc446516cee93f7653a40e1b27184b28df
Dan Solo <dansolo.cc@gmail.com>
2021-08-18 17:27:39 -0500

    Init repo, create class to array operations

---
+++ src/main/kotlin/DS.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // Arrays
+    val sample = OperationSamplesArray()
+    sample.printArray(sample.fiveFives)
+    sample.printArrayForeach(sample.vowels)
+    sample.printArrayForeachIndexed(sample.evenNumbers)
+
+    println("ADD")
+    sample.printArrayForeachIndexed(sample.arrayToAddNewElement)
+    val newArray = sample.insertAt(sample.arrayToAddNewElement, 7)
+    sample.printArrayForeachIndexed(newArray)
+
+    println("UPDATE")
+    sample.printArrayForeachIndexed(sample.arrayToUpdateElement)
+    sample.update(2, 100)
+    sample.printArrayForeachIndexed(sample.arrayToUpdateElement)
+
+    println("RESEARCH")
+    val exist = sample.findElement(sample.arrayToUpdateElement, 9)
+    if (exist) println("Value find") else println("Value isn't exists")
+
+    val reversedVowels = sample.vowels.reversedArray()
+    sample.printArray(reversedVowels)
+
+}
---
+++ src/main/kotlin/OperationSamplesArray.kt
@@ -0,0 +1,92 @@
+class OperationSamplesArray {
+
+    // CREATE
+
+    /**
+     * Create an array
+     *
+     * The easiest way to create an array is by using a function from the Kotlin standard library, arrayOf().
+     * This is a concise way to provide array values.
+     * As with any type, it’s good practice to declare arrays that aren't going to change as constants using val
+     */
+    val vowels = arrayOf("a", "e", "i", "o", "u")
+    /*
+        Since the array only contains integers, Kotlin infers the type of evenNumbers to be an array of Int values.
+        This type is written as Array<Int>
+        val evenNumbers = intArrayOf(2, 4, 6, 8)
+     */
+    val evenNumbers = arrayOf(2, 4, 6, 8)
+    //It’s also possible to create an array with all of its values set to a default value:
+    val fiveFives = Array(5) { 5 } // 5, 5, 5, 5, 5
+
+    // READ
+    /**
+     * Go through all the elements, one by one
+     */
+    fun printArray(array: Array<*>) {
+        for (idx in array) {
+            print(idx)
+        }
+        println()
+    }
+
+
+    fun printArrayForeach(array: Array<*>) {
+        array.forEach { element ->
+            print(element)
+        }
+        println()
+    }
+
+    fun printArrayForeachIndexed(array: Array<*>) {
+        array.forEachIndexed { index, element ->
+            println("Index: $index - Element: $element")
+        }
+        println()
+    }
+
+    // INSERT
+    /**
+     * To insert a new element in an array we can't do it in a direct way because when we use arrayOf, intArrayOf, etc,
+     * because we create an in mutable array.
+     * To do that we need convert array to mutable list with toMutableList() function.
+     * Or we can create a function to return a new array and override our array val, but inside we use toMutableList() function.
+     */
+    val arrayToAddNewElement = arrayOf(0, 1, 2, 3, 4, 5, 6)
+
+    fun insertAt(array: Array<*>, value: Int): Array<*> {
+        val result = array.toMutableList()
+        result.add(value)
+        return result.toTypedArray()
+    }
+
+    // UPDATE
+    /**
+     * To update a value within an array
+     */
+    val arrayToUpdateElement = arrayOf(0, 1, 2, 3, 4, 5, 6)
+    fun update(position: Int, newValue: Int) {
+        if (position < arrayToUpdateElement.size) {
+            arrayToUpdateElement[position] = newValue
+        } else {
+            throw Exception("Index out")
+        }
+
+    }
+
+    // RESEARCH
+    /**
+     * To do a research within an array we need give value to research and we need iterate all array positions
+     */
+    fun findElement(array: Array<Int>, value: Int): Boolean {
+        var existValue = false
+        array.forEach { number ->
+            if (number == value) {
+                existValue = true
+            }
+        }
+        return existValue
+    }
+
+
+}
